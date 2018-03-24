c7b3097f68edd04d9adede1c30a20f152583691e
Alexander Isaev <IsaevAV27@gmail.com>
2018-03-25 00:05:24 +0300

    Implement CSV input reader

---
+++ src/main/java/task/loans/csv/CsvInputReader.java
@@ -0,0 +1,111 @@
+package task.loans.csv;
+
+import java.io.File;
+import java.io.FileReader;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import au.com.bytecode.opencsv.CSVParser;
+import au.com.bytecode.opencsv.CSVReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import task.loans.model.LendingOffer;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Reader extracting market data from CSV file.
+ */
+@ParametersAreNonnullByDefault
+public class CsvInputReader {
+
+    private static final Logger logger = LoggerFactory.getLogger(CsvInputReader.class);
+
+    private final boolean skipFirstLine;
+    private final char separator;
+
+    /**
+     * Create new reader instance with default CSV separator.
+     *
+     * @param skipFirstLine flag, if {@code true} – the first line of CSV file will not be read
+     * @see CSVParser#DEFAULT_SEPARATOR
+     */
+    public CsvInputReader(boolean skipFirstLine) {
+        this(skipFirstLine, null);
+    }
+
+    /**
+     * Create new reader instance.
+     *
+     * @param skipFirstLine flag, if {@code true} – the first line of CSV file will not be read
+     * @param separator     CSV file cells separator, if {@code null},
+     *                      the default separator will be used – {@link CSVParser#DEFAULT_SEPARATOR}
+     */
+    public CsvInputReader(boolean skipFirstLine, @Nullable Character separator) {
+        this.skipFirstLine = skipFirstLine;
+        this.separator = Optional.ofNullable(separator).orElse(CSVParser.DEFAULT_SEPARATOR);
+    }
+
+    /**
+     * Read market data from the input CSV file.
+     *
+     * @param csvFile input file to read data from
+     * @return List of lending offers.
+     * @see LendingOffer
+     * @throws RuntimeException if any of these situations occurs:
+     * <ul>
+     *  <li>file not found</li>
+     *  <li>IO exception caught</li>
+     *  <li>data format is incorrect</li>
+     * </ul>
+     */
+    public List<LendingOffer> read(File csvFile) {
+        FileReader fileReader = onExceptionRethrow(() -> new FileReader(csvFile),
+                "File not found: " + csvFile.getName());
+
+        CSVReader csvReader = new CSVReader(fileReader, separator,
+                CSVParser.DEFAULT_QUOTE_CHARACTER, skipFirstLine ? 1 : 0);
+
+        return readAll(csvReader).stream().map(this::convert).collect(toList());
+    }
+
+    private List<String[]> readAll(CSVReader reader) {
+        return onExceptionRethrow(reader::readAll, "Could not read input CSV file");
+    }
+
+    private LendingOffer convert(String[] row) {
+        checkArgument(row.length == 3, "Invalid row length. Row: %s",
+                stream(row).collect(joining(",", "[", "]")));
+
+        return onExceptionRethrow(() -> new LendingOffer(row[0], new BigDecimal(row[1]), new BigDecimal(row[2])),
+                "Improper input data format");
+    }
+
+    private static <T> T onExceptionRethrow(MethodCall<T> methodCall, String failMessage) {
+        try {
+            return methodCall.get();
+        } catch (Exception ex) {
+            logger.error(failMessage);
+            throw new InternalException(ex);
+        }
+    }
+
+    private interface MethodCall<T> {
+        @SuppressWarnings("squid:S00112")   // generic exception usage
+        T get() throws Exception;
+    }
+
+    private static class InternalException extends RuntimeException {
+        InternalException(Exception cause) {
+            super(cause);
+        }
+    }
+
+}
---
+++ src/test/java/task/loans/csv/CsvInputReaderTest.java
@@ -0,0 +1,73 @@
+package task.loans.csv;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import task.loans.model.LendingOffer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
+
+@ParametersAreNonnullByDefault
+public class CsvInputReaderTest {
+
+    private final File valid = getResourceAsFile("valid.csv");
+    private final File validNoHeader = getResourceAsFile("valid_no_header.csv");
+    private final File validTabSep = getResourceAsFile("valid_tab_sep.csv");
+
+    @Test
+    public void valid_skipLine_parsesFine() {
+        CsvInputReader reader = new CsvInputReader(true);
+        List<LendingOffer> offers = reader.read(valid);
+        assertThat(offers, notNullValue());
+        assertThat(offers, hasSize(7));
+    }
+
+    @Test
+    public void validNoHeader_resultEqualsToValid() {
+        List<LendingOffer> expected = new CsvInputReader(true).read(valid);
+        List<LendingOffer> actual = new CsvInputReader(false).read(validNoHeader);
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test
+    public void validTabSep_resultEqualsToValid() {
+        List<LendingOffer> expected = new CsvInputReader(true).read(valid);
+        List<LendingOffer> actual = new CsvInputReader(true, '\t').read(validTabSep);
+        assertThat(actual, equalTo(expected));
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    public void fileNotFound_throwsException() {
+        new CsvInputReader(false).read(new File("non.existent"));
+    }
+
+    @Test(dataProvider = "invalidCSVs", expectedExceptions = RuntimeException.class)
+    public void invalidCsvFiles_throwsException(File csv) {
+        new CsvInputReader(false).read(csv);
+    }
+
+    @DataProvider(name = "invalidCSVs")
+    public static Iterator<Object> invalid() {
+        return Stream.of(
+                "invalid_1.csv",
+                "invalid_2.csv",
+                "invalid_3.csv",
+                "invalid_4.csv")
+                .map(res -> (Object) getResourceAsFile(res))
+                .iterator();
+    }
+
+    private static File getResourceAsFile(String resourceName) {
+        return new File(CsvInputReaderTest.class.getResource(resourceName).getFile());
+    }
+
+}
